02f685db7bd050af7485d446360fad1bcd8a4116
Wugoho <c.s.n@live.de>
2019-04-06 19:21:08 +0200

    Main calls a fct in the package GlobalPrinter

--- main/Main.java
+++ main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String... args){
 
-		Globalprinter prints = new GlobalPrinter();
+		GlobalPrinter prints = new GlobalPrinter();
 		prints.printer();
 
 	}
